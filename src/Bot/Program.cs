ee2c3292b50ca64bcd497e86694dbd38985c1d09
Roman Zhdanov <zhdanov.roma@gmail.com>
2025-06-01 17:34:42 +0400

    Initial project.

---
+++ src/Bot/Program.cs
@@ -0,0 +1,13 @@
+namespace LakeRun.Bot;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddHostedService<Worker>();
+
+        var host = builder.Build();
+        await host.RunAsync();
+    }
+}
